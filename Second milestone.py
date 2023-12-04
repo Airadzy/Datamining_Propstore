cd5244f32efed467fba3c1cdb92f3b5a143b901c
Adrian <airadzy@gmail.com>
2023-12-04 11:00:38 +0100

    added function that goes through each category to be able to have different categories

--- Second milestone.py
+++ Second milestone.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
+import urllib
 
 
 def get_page_content(driver):
@@ -27,7 +28,7 @@ def extract_data(html_content):
             card_title = " ".join(card_title.split())
             price = card.find("span", class_="card__price-title").text.strip()
             card_dict[number] = [movie_name,card_title,price]
-            print(f"Movie: {movie_name}, Title: {card_title}, Price: {price}")
+            # print(f"Movie: {movie_name}, Title: {card_title}, Price: {price}")
         except Exception as error:
             print("error")
             continue
@@ -56,11 +57,11 @@ def scroll_to_bottom(driver):
     time.sleep(1)
 
 
-def login(driver, username, password):  # TODO update login method
-    """function to login to website (IN CONSTRUCTION)"""
+def login(driver, username, password):
+    """function to login to website """
     signin_button = driver.find_element(By.XPATH, '//a[text()="Sign In"]')
     signin_button.click()
-    print("sign in window opened)")
+
 
     WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '//input[@id="email"]')))
     username_field = driver.find_element(By.XPATH,'//input[@id="email"]')
@@ -72,14 +73,13 @@ def login(driver, username, password):  # TODO update login method
     # //*[@id="modal-signin"]/form/button[2]
 
     # login_button = driver.find_element(By.XPATH, '//button[@id="submit"]')
-    print("button found")
     login_button.click()
     WebDriverWait(driver, 10)
 
 def scroll_website(driver):
     """function to check if we can scroll down further on website (via can_scroll function) and if True, scroll down website (via scroll_to_bottom function). Next
      get all prop data from get_page_content function and then take relevant info via extract_data function before quitting"""
-    login(driver, "a.radzyminski@icloud.com", "ITC_Dataextract_2023")  # TODO: update login method
+    login(driver, "a.radzyminski@icloud.com", "ITC_Dataextract_2023")
     time.sleep(10)
     try:
         i = 0
@@ -95,12 +95,24 @@ def scroll_website(driver):
         time.sleep(10)
         driver.quit()
 
+def go_through_categories(url):
+    """docstring placeholder"""
+    category_list = ["props", "costumes", "artwork", "posters", "toys", "autographs", "promotional-items"]
+    category_url_list = [url.replace("products", f"category/{category}") for category in category_list]
+
+    return category_url_list
+
+
+
+
+
+
 
 def main():
     """main function to run through propstore.com, scroll to the bottom of the page and take movie information"""
     url = "https://propstore.com/products/?sortType=5&buyNow=1&archive=1&scroll=220"
     driver = webdriver.Chrome()  # initialize webbrowser (Chrome in this instance)
-    print("1",driver)
+    go_through_categories(url)
     driver.get(url)
     scroll_website(driver)
 
