4b1dfb5f9200401fb0e84fae58cc81e7078d065d
Adrian <airadzy@gmail.com>
2023-12-05 18:19:38 +0100

    updated extraction function to extract into list instead of dict

--- Extract_data_function.py
+++ Extract_data_function.py
@@ -1,16 +1,24 @@
 from bs4 import BeautifulSoup
 import logging
+import csv
+
+def extract_to_csv(items_list):
+    header_names = ["id","category","movie_name","card_title","price"]
+    with open("Propstore_data.csv","w") as propstore_csv_file:
+        writer = csv.DictWriter(propstore_csv_file,fieldnames=header_names)
+        writer.writeheader()
+        for row in reader
 
 def extract_data(html_content, category_url):
     """
-    extract movie name, sale date and price (either sold or offer if live) from website
+    Extract movie name, sale date and price (either sold or offer if live) from website
     :param html_content:
     :param category_url:
     :return:
     """
     soup = BeautifulSoup(html_content, "html.parser")
     cards = soup.find_all("div", class_="card__info")
-    card_dict = {}
+    items_list = []
     for number, card in enumerate(cards):
         try:
             category = category_url.split("/")[4]
@@ -18,15 +26,18 @@ def extract_data(html_content, category_url):
             card_title = card.find("div", class_="card__title").text.strip()
             card_title = " ".join(card_title.split())
             price = card.find("span", class_="card__price-title").text.strip()
-            card_dict[number] = [category, movie_name, card_title, price]
+            item_dict = {"category": {category},"movie_name":{movie_name},"card_title":{card_title},"price": {price}}
+            items_list.append(item_dict)
         except Exception as error:
             print(f"error {error}")
             continue
     logging.info(f"\nCreated dict from {category} with: {len(card_dict)} items\n")
-    print(card_dict)
+    print(items_list)
+
 
 def main():
-    extract_data(input1,input2)
+    print("No relevance to this function standalone")
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     main()
--- Selenium_functions.py
+++ Selenium_functions.py
@@ -8,28 +8,33 @@ import logging
 import time
 import Extract_data_function
 
+
 def can_scroll(driver):
     """
-    function to check that website can be scrolled down further
-    :param driver:
-    :return:
+    Checks if the user has scrolled to the bottom of the page or not.
+    :param driver: driver with url for each category site
+    :return: n/a
     """
 
     return driver.execute_script(
         "return window.scrollY + window.innerHeight < document.body.scrollHeight;")  # scroll down completely body
+
+
 def get_page_content(driver):
     """
-
+    Function to return the HTML source code of the currently loaded web page through the driver object.
     :param driver: driver with website url
     :return: page content in text format
     """
     logging.info(f"Fetching page content via {driver}")
     return driver.page_source
+
+
 def scroll_to_bottom(driver):
     """
-    function to scroll down website
-    :param driver:
-    :return:
+    Function to go to bottom of website.
+    :param driver: driver with url for each category site
+    :return: n/a
     """
 
     actions = ActionChains(driver)
@@ -39,34 +44,39 @@ def scroll_to_bottom(driver):
 
 def login(driver, username, password):
     """
-    function to log in to website
-    :param driver:
-    :param username:
-    :param password:
-    :return:
+    Function to log in to Propstore website using username / password credentials.
+    :param driver: driver with url for each category site
+    :param username: Propstore username
+    :param password: Propstore website
+    :return: n/a
     """
 
-    signin_button = driver.find_element(By.XPATH, '//a[text()="Sign In"]')
-    signin_button.click()
+    try:
+        signin_button = driver.find_element(By.XPATH, '//a[text()="Sign In"]')
+        signin_button.click()
 
-    WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '//input[@id="email"]')))
-    username_field = driver.find_element(By.XPATH, '//input[@id="email"]')
-    username_field.send_keys(username)
-    password_field = driver.find_element(By.XPATH, '//input[@id="password"]')
-    password_field.send_keys(password)
+        WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, '//input[@id="email"]')))
+        username_field = driver.find_element(By.XPATH, '//input[@id="email"]')
+        username_field.send_keys(username)
+        password_field = driver.find_element(By.XPATH, '//input[@id="password"]')
+        password_field.send_keys(password)
 
-    login_button = WebDriverWait(driver, 30).until(
-        ec.element_to_be_clickable((By.CSS_SELECTOR, 'button.modal-register__submit')))
-    login_button.click()
-    WebDriverWait(driver, 10)
+        login_button = WebDriverWait(driver, 30).until(
+            ec.element_to_be_clickable((By.CSS_SELECTOR, 'button.modal-register__submit')))
+        login_button.click()
+        WebDriverWait(driver, 10)
+    except Exception as error:
+        print(f"Error: {error}")
+
+
 
 def process_category(category_url, username, password):
     """
-    [PLACEHOLDER]
-    :param category_url:
-    :param username:
-    :param password:
-    :return:
+    Function to open a url in Selenium and act on it by calling the next function that scrolls through it.
+    :param category_url: url of category site (e.g. toys)
+    :param username: Propstore username
+    :param password: Propstore password
+    :return: n/a
     """
     driver = webdriver.Chrome()
     driver.get(category_url)
@@ -75,26 +85,26 @@ def process_category(category_url, username, password):
 
 def scroll_website(driver, username, password, category_url):
     """
-    function to check if we can scroll down further on website (via can_scroll function) and if True, scroll down website (via scroll_to_bottom function). Next
-    get all prop data from get_page_content function and then take relevant info via extract_data function before quitting
-    :param driver:
-    :param username:
-    :param password:
-    :param category_url:
-    :return:
+    Function to log in to website, scroll down website as long as possible and extract key information.
+    This is executed by calling other functions.
+    :param driver: driver with url for each category site
+    :param username: Propstore username
+    :param password: Propstore password
+    :param category_url: url of category site (e.g. toys)
+    :return: n/a
     """
 
     login(driver, username, password)
     logging.info(f"Ran function {login(driver, username, password)}")
     time.sleep(3)
     try:
-        i = 0
-        while can_scroll(driver) and i < 3:
+        # i = 0
+        while can_scroll(driver):  # and i < 3
             scroll_to_bottom(driver)
             html_content = get_page_content(driver)
             Extract_data_function.extract_data(html_content, category_url)
             WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.TAG_NAME, "body")))
-            i += 1
+            # i += 1
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
